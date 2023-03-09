d8ac3495e27cbd67480d4b871b263bb5dd1d615c
NQuocBao <nguyenquocbao9483@gmail.com>
2023-03-09 13:51:15 +0700

    BaiTap

--- drawing-menu/src/main/java/com/codegym/Main.java
+++ drawing-menu/src/main/java/com/codegym/Main.java
@@ -9,7 +9,7 @@ public class Main {
         while(choice != 0) {
             System.out.println("Menu");
             System.out.println("1. Draw the triangle");
-            System.out.println("2. Draw the square");
+            System.out.println("2. Draw the square triangle");
             System.out.println("3. Draw the rectangle");
             System.out.println("0. Exit");
             System.out.println("Enter your choice: ");
